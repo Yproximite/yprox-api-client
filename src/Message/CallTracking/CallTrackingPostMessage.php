9f90c923e11e910e374bee61238aaeff7b4b1c1a
Elodie Nazaret <elodie@yproximite.com>
2017-04-21 16:31:10 +0200

    Gestion CallerId CallTracking

--- src/Message/CallTracking/CallTrackingPostMessage.php
+++ src/Message/CallTracking/CallTrackingPostMessage.php
@@ -3,79 +3,11 @@ declare(strict_types=1);
 
 namespace Yproximite\Api\Message\CallTracking;
 
-use Yproximite\Api\Message\MessageInterface;
-use Yproximite\Api\Message\SiteAwareMessageTrait;
-
 /**
  * Class CallTrackingPostMessage
  */
-class CallTrackingPostMessage implements MessageInterface
+class CallTrackingPostMessage extends AbstractCallTrackingMessage
 {
-    use SiteAwareMessageTrait;
-
-    /**
-     * @var string
-     */
-    private $name;
-
-    /**
-     * @var string|null
-     */
-    private $phoneDid;
-
-    /**
-     * @var string
-     */
-    private $phoneDestination;
-
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name
-     */
-    public function setName(string $name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return null|string
-     */
-    public function getPhoneDid()
-    {
-        return $this->phoneDid;
-    }
-
-    /**
-     * @param null|string $phoneDid
-     */
-    public function setPhoneDid(string $phoneDid = null)
-    {
-        $this->phoneDid = $phoneDid;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPhoneDestination(): string
-    {
-        return $this->phoneDestination;
-    }
-
-    /**
-     * @param string $phoneDestination
-     */
-    public function setPhoneDestination(string $phoneDestination)
-    {
-        $this->phoneDestination = $phoneDestination;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -85,6 +17,7 @@ class CallTrackingPostMessage implements MessageInterface
             'name'             => $this->getName(),
             'phoneDid'         => $this->getPhoneDid(),
             'phoneDestination' => $this->getPhoneDestination(),
+            'callerId'         => $this->hasCallerId(),
         ];
     }
 }
